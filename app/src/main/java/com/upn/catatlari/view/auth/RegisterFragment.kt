d236a9b2865d4fd47b00ceda90d39a68d5ae058e
firzalay <firza7004@gmail.com>
2026-04-29 20:46:59 +0700

    style: restyling register page

--- app/src/main/java/com/upn/catatlari/view/auth/RegisterFragment.kt
+++ app/src/main/java/com/upn/catatlari/view/auth/RegisterFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.upn.catatlari.databinding.FragmentRegisterBinding
+import com.upn.catatlari.utils.setClickAnimation
 
 class RegisterFragment : Fragment() {
 
@@ -17,5 +19,22 @@ class RegisterFragment : Fragment() {
 
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvRegister.setClickAnimation {
+            findNavController().navigate(
+                RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+            )
+        }
+
+        binding.btnRegister.setClickAnimation {
+            findNavController().navigate(
+                RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+            )
+        }
+    }
+
 
 }
